87636165d8194455c5c86cc4164cf2533c232df0
soumya12062013 <soumya.dec2006@gmail.com>
2025-10-08 15:11:20 +0530

    home_screen_created

--- lib/screens/Home_screen.dart
+++ lib/screens/Home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Quiz_Screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,11 +11,71 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Welcome to Brain Lab",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/homescreeni_mg.png.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Quizz_screen()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 52, 177, 235),
+                foregroundColor: Colors.white10,
+                padding: EdgeInsets.all(10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              child: Text(
+                "START A NEW GAME",
+                style: TextStyle(
+                  //color: Colors.white
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 26, 175, 239),
+                foregroundColor: Colors.white10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              child: Text(
+                "LEADERBOARD",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 52, 177, 235),
+                foregroundColor: Colors.white10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              child: Text(
+                "SETTINGS",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/screens/quiz_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Quizz_screen extends StatefulWidget {
+  const Quizz_screen({super.key});
+
+  @override
+  State<Quizz_screen> createState() => _Quizz_screenState();
+}
+
+class _Quizz_screenState extends State<Quizz_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
